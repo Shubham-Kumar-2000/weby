5d6a6559870753983e1e4ef003a548850470f76c
Shubham-Kumar-2000 <shukhu10@gmail.com>
2020-02-23 00:40:03 +0530

    color

--- src/list.js
+++ src/list.js
@@ -20,7 +20,7 @@ constructor(props)
     url:'https://images.unsplash.com/photo-1541018939203-36eeab6d5721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
     name:'Project Lorem Epsum',
     status:"Work in Progress",
-    st:'yellow',
+    st:'#ffbb00',
     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },{
     url:'https://images.unsplash.com/photo-1541018939203-36eeab6d5721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
